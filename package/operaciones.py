8daa9207e5e5ca0b34321909e8b93494c6eea05c
Joshua <Joshua4yala@pm.me>
2020-11-29 10:14:04 -0800

    Add files via upload

---
+++ package/operaciones.py
@@ -0,0 +1,15 @@
+def suma(x,y, resultado):
+	resultado = x + y
+	return resultado
+
+def resta(x,y, resultado):
+	resultado = x - y
+	return resultado
+
+def circulo(radio, pi, resultado):
+	resultado = pi*radio**2
+	return resultado
+
+def rectangulo(base, altura, resultado):
+	resultado = base*altura
+	return resultado
